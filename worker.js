807db29c45538bdc325ba00474c4d02e99ac15f3
xander-haj <114783946+xander-haj@users.noreply.github.com>
2024-12-05 05:31:04 -0600

    Update worker.js

--- worker.js
+++ worker.js
@@ -3,16 +3,30 @@
 
 importScripts('https://cdn.jsdelivr.net/npm/tesseract.js@2/dist/tesseract.min.js');
 
+let worker;
+
+// Initialize worker with a specific number of threads
 onmessage = function (e) {
-    let { imageData, lang } = e.data;
-    Tesseract.recognize(
-        imageData,
-        lang,
-        { logger: m => console.log(m) }
-    ).then(({ data: { text } }) => {
-        postMessage({ text });
-    }).catch(err => {
-        console.error(err);
-        postMessage({ text: 'Error: ' + err.message });
-    });
+    if (e.data.cmd === 'init') {
+        let numWorkers = e.data.numWorkers || 1;
+        Tesseract.create({
+            workerPath: 'https://cdn.jsdelivr.net/npm/tesseract.js@2/dist/worker.min.js',
+            langPath: 'https://cdn.jsdelivr.net/npm/tesseract.js@2/dist/lang/',
+            corePath: 'https://cdn.jsdelivr.net/npm/tesseract.js-core@2/engines/tesseract-core.wasm.js',
+            workerBlobURL: false,
+            workerOptions: { numWorkers }
+        });
+    } else {
+        let { imageData, lang } = e.data;
+        Tesseract.recognize(
+            imageData,
+            lang,
+            { logger: m => console.log(m) }
+        ).then(({ data: { text } }) => {
+            postMessage({ status: 'result', text });
+        }).catch(err => {
+            console.error(err);
+            postMessage({ status: 'error', text: 'Error: ' + err.message });
+        });
+    }
 };
